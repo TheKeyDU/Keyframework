1f013121d097ea63680a657907142a0eaeca3b9a
Administrator <key>
2019-11-04 16:45:15 +0800

    create demo adapter

---
+++ app/src/main/java/com/example/keyframework/adapter/DemoAdapter.kt
@@ -0,0 +1,13 @@
+package com.example.keyframework.adapter
+
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.BaseViewHolder
+import com.example.keyframework.R
+import com.example.keyframework.bean.UserBean
+
+class DemoAdapter(data: MutableList<UserBean>?) : BaseQuickAdapter<UserBean, BaseViewHolder>(R.layout.activity_main, data) {
+    override fun convert(helper: BaseViewHolder?, item: UserBean?) {
+        helper?.setText(R.id.demo, item?.name)
+
+    }
+}
---
+++ app/src/main/java/com/example/keyframework/bean/UserBean.java
@@ -0,0 +1,27 @@
+package com.example.keyframework.bean;
+
+public class UserBean {
+    public UserBean(String name, String id) {
+        this.name = name;
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    private String name;
+    private String Id;
+}
