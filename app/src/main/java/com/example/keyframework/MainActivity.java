5d656d975c22b304111ddb877660e6a105e31364
Administrator <key>
2019-11-06 17:11:12 +0800

    frist page

--- app/src/main/java/com/example/keyframework/MainActivity.java
+++ app/src/main/java/com/example/keyframework/MainActivity.java
@@ -3,12 +3,6 @@ package com.example.keyframework;
 
 
 
-
-import android.os.Bundle;
-import android.os.PersistableBundle;
-
-import androidx.annotation.Nullable;
-
 import com.maosong.component.Base.BaseActivity;
 
 public class MainActivity extends BaseActivity {
