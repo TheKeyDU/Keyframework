c90ee23eda6d9bb0e9dc071f3dc0a31b023ae002
Administrator <key>
2019-11-13 11:01:36 +0800

    加了个抽屉

--- app/src/main/java/com/example/keyframework/activity/MainActivity.java
+++ app/src/main/java/com/example/keyframework/activity/MainActivity.java
@@ -5,21 +5,26 @@ import com.example.keyframework.Constants.ARouterPage;
 import com.example.keyframework.R;
 import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 import com.maosong.component.Base.BaseActivity;
 import com.maosong.tools.AppLifeCircleUtil;
 import androidx.annotation.NonNull;
-import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import android.animation.Animator;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.view.animation.AccelerateInterpolator;
 
-
 @Route(path = ARouterPage.MAIN_ACTIVITY)
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
     Animator animator = null;
-    ConstraintLayout constraintLayout = null;
+    androidx.drawerlayout.widget.DrawerLayout mDrawerLayout = null;
     BottomNavigationView navView = null;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -45,24 +50,32 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void initView() {
-        navView = findViewById(R.id.nav_view);
-        constraintLayout = findViewById(R.id.container);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        mDrawerLayout = findViewById(R.id.dwl_root);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, mDrawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+        navView = findViewById(R.id.nav_bot);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         AppLifeCircleUtil.getInstance().finishActivity(AppLifeCircleUtil.activityStack.get(0));
     }
 
     @Override
     public void initDate() {
-        constraintLayout.postDelayed(new Runnable() {
+        mDrawerLayout.postDelayed(new Runnable() {
             @Override
             public void run() {
-                startCircularReveal(constraintLayout, constraintLayout.getWidth() / 2, 0, constraintLayout.getHeight(), 700);
+                startCircularReveal(mDrawerLayout, mDrawerLayout.getWidth() / 2, 0, mDrawerLayout.getHeight(), 700);
             }
         }, 100);
     }
 
     private void itemViewStartAnimation(View view) {
-        startCircularReveal(constraintLayout, (int) view.getX() + view.getWidth() / 2, constraintLayout.getHeight(), constraintLayout.getHeight(), 200);
+        startCircularReveal(mDrawerLayout, (int) view.getX() + view.getWidth() / 2, mDrawerLayout.getHeight(), mDrawerLayout.getHeight(), 200);
     }
 
     private void startCircularReveal(View view,
@@ -85,7 +98,7 @@ public class MainActivity extends BaseActivity {
 
             @Override
             public void onAnimationEnd(Animator animation) {
-                constraintLayout.setVisibility(View.VISIBLE);
+                mDrawerLayout.setVisibility(View.VISIBLE);
 
             }
 
@@ -104,13 +117,69 @@ public class MainActivity extends BaseActivity {
 
     }
 
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
     @Override
     public int getContentViewRes() {
-        return R.layout.activity_mainbot;
+        return R.layout.activity_main;
     }
 
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        // Handle navigation view item clicks here.
+        int id = menuItem.getItemId();
+
+        if (id == R.id.nav_home) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_tools) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = findViewById(R.id.dwl_root);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.dwl_root);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            onBackPressed();
+        }
     }
 }
