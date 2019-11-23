f0f5c55c0b07d58aeff1719d9a99bc589fc28d40
Administrator <key>
2019-11-23 14:24:14 +0800

    save URL

--- app/src/main/java/com/example/keyframework/constants/EnvironmentConstants.java
+++ app/src/main/java/com/example/keyframework/constants/EnvironmentConstants.java
@@ -2,4 +2,8 @@ package com.example.keyframework.constants;
 
 public class EnvironmentConstants {
     public static String ITHOME_URL="https://api.ithome.com/";
+    public static String ITHOME_URL_WITH_PAGES="https://m.ithome.com/api/news/";
+    public static String PAGE="newslistpageget?Tag=&ot=1574472961000&page=0";
+
+
 }
